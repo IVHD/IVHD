2f257d38093ca2ddce413b62bc56c27bb9922b08
Błażej Nowicki <blazej.nowicki@example.org>
2024-04-29 14:40:21 +0200

    Initial setup

---
+++ ivhd/__init__.py
@@ -0,0 +1,5 @@
+from .test import test
+
+__all__ = [
+    "test",
+]
---
+++ ivhd/test.py
@@ -0,0 +1,6 @@
+import sklearn
+
+
+def test():
+    print(f"scikit-learn=={sklearn.__version__}")
+    print("Everything is setup correctly. 🔥")
